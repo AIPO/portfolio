23cb77328efabca35eecce19d36727f781cb3631
AIPO <p.vaitonis@gmail.com>
2019-11-03 22:27:31 +0200

    Thread and reply models + seeders for fake data.

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,16 +2,18 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\Thread;
+use App\Models\Reply;
 use App\Models\User;
 use Faker\Generator as Faker;
 
-$factory->define(Thread::class, function (Faker $faker) {
+$factory->define(Reply::class, function (Faker $faker) {
     return [
         'user_id' => function () {
             return factory(User::class)->create()->id;
         },
-        'title' => $faker->sentence,
+        'thread_id'=>function () {
+            return factory(User::class)->create()->id;
+        },
         'content' => $faker->paragraphs
 
     ];
